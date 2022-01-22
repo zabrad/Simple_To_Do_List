25441994a84cbb2bced1625b3c98fa6e03349ad6
SupercraftD <dzhu700@gmail.com>
2022-01-22 16:53:28 -0500

    made comments for function and made it bigger
    I felt like contributing to some random repo but didn't want to do much work so I decided to add some very simple comments for your functions lol. Also made it bigger by default for my eyes.

--- Simple_To_Do_List.py
+++ Simple_To_Do_List.py
@@ -1,19 +1,26 @@
+#import necessary libraries
 import tkinter
 import tkinter.messagebox
 import pickle
 
+#start root app
 root = tkinter.Tk()
 root.title("To-Do-List by @BradRedondo")
 
 def add_task():
+    #gets task from entry
     task = entry_task.get()
+
+    #shows warning if task is empty
     if task != "":
         listbox_tasks.insert(tkinter.END, task)
         entry_task.delete(0, tkinter.END)
     else:
         tkinter.messagebox.showwarning(title = "Warning", message = "You must enter a task. ")
 
+
 def delete_task():
+    #deletes selected task, shows warning if not selected.
     try:
         task_index = listbox_tasks.curselection()[0]
         listbox_tasks.delete(task_index)
@@ -21,6 +28,7 @@ def delete_task():
         tkinter.messagebox.showwarning(title="Warning!", message="You must select a task.")
 
 def load_tasks():
+    #loads tasks from "tasks.dat", shows warning if not found.
     try:
         tasks = pickle.load(open("tasks.dat", "rb"))
         listbox_tasks.delete(0, tkinter.END)
@@ -30,35 +38,38 @@ def load_tasks():
         tkinter.messagebox.showwarning(title="Warning!", message="Cannot find tasks.dat.")
 
 def save_tasks():
+    #saves tasks in tasks.dat
     tasks = listbox_tasks.get(0, listbox_tasks.size())
     pickle.dump(tasks, open("tasks.dat", "wb"))
 
 # Create GUI
+
 frame_tasks = tkinter.Frame(root)
 frame_tasks.pack()
 
-listbox_tasks = tkinter.Listbox(frame_tasks, height = 10, width = 50)
+listbox_tasks = tkinter.Listbox(frame_tasks, height = 30, width = 150)
 listbox_tasks.pack(side = tkinter.LEFT)
 
 scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
 scrollbar_tasks.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 
+
 listbox_tasks.config(yscrollcommand = scrollbar_tasks.set)
 scrollbar_tasks.config(command = listbox_tasks.yview)
 
-entry_task = tkinter.Entry(root, width = 50)
+entry_task = tkinter.Entry(root, width = 150)
 entry_task.pack()
 
-button_add_task = tkinter.Button(root, text = "Add task", width = 48, command = add_task)
+button_add_task = tkinter.Button(root, text = "Add task",height = 2, width = 130, command = add_task)
 button_add_task.pack()
 
-button_delete_task = tkinter.Button(root, text = "Delete task", width = 48, command = delete_task)
+button_delete_task = tkinter.Button(root, text = "Delete task",height = 2, width = 130, command = delete_task)
 button_delete_task.pack()
 
-button_load_tasks = tkinter.Button(root, text = "Load tasks", width = 48, command = load_tasks)
+button_load_tasks = tkinter.Button(root, text = "Load tasks",height = 2, width = 130, command = load_tasks)
 button_load_tasks.pack()
 
-button_save_tasks = tkinter.Button(root, text = "Save tasks", width = 48, command = save_tasks)
+button_save_tasks = tkinter.Button(root, text = "Save tasks",height = 2, width = 130, command = save_tasks)
 button_save_tasks.pack()
 
 root.mainloop()
